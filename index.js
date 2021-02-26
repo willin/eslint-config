6bb127f1b6d156ff43674be8bf0baa94b2143b87
Willin Wang <willin@willin.org>
2021-02-26 13:59:49 +0800

    fix: prettier 8.0 config

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint'
+    'prettier'
   ],
   env: {
     node: true,
